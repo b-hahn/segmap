4fb8c92e5a0403d6d9becd47d2d3d756f2c180d6
Benjamin Hahn <hahnb@student.ethz.ch>
2019-02-05 17:26:15 +0100

    support for color points now uses inheritance and works

--- segmatch/include/segmatch/search/point_representation_spec.hpp
+++ segmatch/include/segmatch/search/point_representation_spec.hpp
@@ -8,37 +8,102 @@
 
 namespace pcl {
 
-template<>
-class DefaultPointRepresentation<segmatch::PointExtended> : public PointRepresentation<segmatch::PointExtended>
-// class DefaultPointRepresentation<segmatch::MapPoint> : public PointRepresentation<segmatch::MapPoint>
-{
-  public:
-    DefaultPointRepresentation()
-    {
-        nr_dimensions_ = 4;
+// template<>
+// class DefaultPointRepresentation<segmatch::PointExtended> : public PointRepresentation<segmatch::PointExtended>
+// // class DefaultPointRepresentation<segmatch::MapPoint> : public PointRepresentation<segmatch::MapPoint>
+// {
+//   public:
+//     DefaultPointRepresentation()
+//     {
+//         nr_dimensions_ = 4;
+//         trivial_ = true;
+//         another_test = 56;
+//         std::cout << "---------------> correct template spec!!!!!!!!!!!!\n";
+//     }
+
+//     // TODO: potentially add functionality to convert rgb to hue here since that's all we need for segmentation. 
+//     virtual void copyToFloatArray(const segmatch::MapPoint& p, float* out) const
+//     {
+//         out[0] = p.x;
+//         out[1] = p.y;
+//         out[2] = p.z;
+//         out[3] = p.rgb;
+//         std::cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ p.rgb = " << out[3];
+//     }
+
+//     void printNumDims() const {
+//         std::cout << "num dims from inside class: " << nr_dimensions_ << std::endl;
+//     }
+
+//     static constexpr int existance = 447;
+//     int another_test = 43;
+// };
+
+
+// derive DefaultPointRepresentation class
+// TODO: maybe create generic base class and create specialization for color and/or color + semantics
+template <typename PointDefault>
+  class SemanticPointRepresentation : public PointRepresentation<PointDefault>
+  {
+    using PointRepresentation<PointDefault>::nr_dimensions_;
+    using PointRepresentation<PointDefault>::trivial_;
+    // using PointRepresentation<PointDefault>::makeShared;
+
+    public:
+      // Boost shared pointers
+      typedef boost::shared_ptr<SemanticPointRepresentation<PointDefault> > Ptr;
+      typedef boost::shared_ptr<const SemanticPointRepresentation<PointDefault> > ConstPtr;
+
+      SemanticPointRepresentation()
+      {
+        nr_dimensions_ = 7;
         trivial_ = true;
-        another_test = 56;
-        std::cout << "---------------> correct template spec!!!!!!!!!!!!\n";
-    }
+      }
 
-    // TODO: potentially add functionality to convert rgb to hue here since that's all we need for segmentation. 
-    virtual void copyToFloatArray(const segmatch::MapPoint& p, float* out) const
-    {
-        out[0] = p.x;
-        out[1] = p.y;
-        out[2] = p.z;
-        out[3] = p.rgb;
-        std::cout << "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ p.rgb = " << out[3];
-    }
+      virtual ~SemanticPointRepresentation () {}
 
-    void printNumDims() const {
-        std::cout << "num dims from inside class: " << nr_dimensions_ << std::endl;
-    }
 
-    static constexpr int existance = 447;
-    int another_test = 43;
-};
-}
+      inline Ptr
+      makeShared () const
+      {
+        return (Ptr (new SemanticPointRepresentation<PointDefault> (*this)));
+      }
 
+      virtual void
+      copyToFloatArray (const PointDefault &p, float * out) const
+      {
+        // If point type is unknown, treat it as a struct/array of floats
+        const float* ptr = reinterpret_cast<const float*> (&p);
+        for (int i = 0; i < nr_dimensions_; ++i)
+          out[i] = ptr[i];
+      }
+  };
+
+
+  template<>
+  class SemanticPointRepresentation<segmatch::MapPoint> : public PointRepresentation<segmatch::MapPoint>
+  {
+    public:
+      SemanticPointRepresentation()
+      {
+          nr_dimensions_ = 4;
+          trivial_ = true;
+      }
+
+      // TODO: potentially add functionality to convert rgb to hue here since that's all we need for segmentation.
+      virtual void copyToFloatArray(const segmatch::MapPoint& p, float* out) const
+      {
+          out[0] = p.x;
+          out[1] = p.y;
+          out[2] = p.z;
+          out[3] = p.rgb;
+      }
+
+      void printNumDims() const { std::cout << "num dims from inside class: " << nr_dimensions_ << std::endl; }
+
+      static constexpr int existance = 447;
+      int another_test = 43;
+  };
+  } // namespace pcl
 
 #endif // SEGMATCH_POINT_REPRESENTATION_SPEC_H_
